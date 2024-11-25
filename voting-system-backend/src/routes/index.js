3008128556696a62aa180be099609318004b19f8
Kelvin Likollari <55785978+likolk@users.noreply.github.com>
2024-11-25 13:37:17 +0100

    added condition in smart contract so that only people with 5% or more can propose changes

--- voting-system-backend/src/routes/index.js
+++ voting-system-backend/src/routes/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
-const { registerVoterController, getProposalVotesController } = require('../controllers/votingControllers');
+const { 
+    registerVoterController, 
+    getProposalVotesController,
+    createProposalController,
+    listAllProposalsController
+
+} = require('../controllers/votingControllers');
+const { route } = require('../app');
 
 const router = express.Router();
 
 // Routes
 router.post('/voter/register', registerVoterController);
 router.get('/proposal/:proposalId/votes', getProposalVotesController);
-
+router.post('/proposal', createProposalController);
+router.get('/proposals', listAllProposalsController);
 module.exports = router;
