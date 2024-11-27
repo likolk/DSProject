b9aa8c9da09b82432b2d8caff382a3ead0e331ff
Kelvin Likollari <55785978+likolk@users.noreply.github.com>
2024-11-27 10:46:29 +0100

    frontend fix

--- voting-system-frontend/src/components/Layout.vue
+++ voting-system-frontend/src/components/Layout.vue
@@ -197,8 +197,7 @@ const route = useRoute();
 const user = {
   name: "Tom Cook",
   email: "tom@example.com",
-  imageUrl:
-    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  imageUrl: ""
 };
 
 // Modify navigation bar: remove "Proposal Creation" and change "Dashboard" to "Home"
