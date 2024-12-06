42c15973f0309927b6338be705ab93ef437944b4
Kelvin Likollari <55785978+likolk@users.noreply.github.com>
2024-12-06 10:46:57 +0100

    getting rewarded after voting

--- voting-system-smart-contracts/contracts/smartContract.sol
+++ voting-system-smart-contracts/contracts/smartContract.sol
@@ -5,17 +5,14 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./VotingWithRewards.sol";
 
 contract VotingContract {
-mapping(address => uint256) public shares; 
-    mapping(address => uint256) public tokens; 
+    mapping(address => uint256) public shares;
+    mapping(address => uint256) public tokens;
     // mapping(uint256 => Proposal) public proposals;
     mapping(address => VotingRecord[]) public votingHistory;
     mapping(uint256 => ProposalOutcome) public proposalOutcomes;
     mapping(address => bool) public admins;
     uint256 public totalShares;
     mapping(address => bool) public voted;
-
-
-
 
     enum QuorumType {
         SimpleMajority,
@@ -24,19 +21,17 @@ mapping(address => uint256) public shares;
         Unanimous
     }
 
-
-struct Proposal {
-    string title;
-    string description;
-    uint256 durationInMinutes;
-    uint8 quorumType;
-    uint256 endTime;
-    address proposer;
-    bool active;
-    uint256 votesFor;  
-    uint256 votesAgainst;
-}
-
+    struct Proposal {
+        string title;
+        string description;
+        uint256 durationInMinutes;
+        uint8 quorumType;
+        uint256 endTime;
+        address proposer;
+        bool active;
+        uint256 votesFor;
+        uint256 votesAgainst;
+    }
 
     Proposal[] public proposals;
     uint256 public proposalCount;
@@ -78,66 +73,71 @@ struct Proposal {
 
     // constructor to initialize admins
     constructor(address[] memory initialAdmins, address _tokenAddress) {
-            governanceToken = GovernanceToken(_tokenAddress);
+        governanceToken = GovernanceToken(_tokenAddress);
 
-            for (uint i = 0; i < initialAdmins.length; i++) {
-                admins[initialAdmins[i]] = true;
-            }
+        for (uint i = 0; i < initialAdmins.length; i++) {
+            admins[initialAdmins[i]] = true;
         }
+    }
 
-        modifier onlyAdmin() {
+    modifier onlyAdmin() {
         require(admins[msg.sender], "Only admins can perform this action");
         _;
     }
 
-function createProposal(
-    string memory title,
-    string memory description,
-    uint256 durationInMinutes,
-    uint8 quorumType
-) public payable {
-    require(msg.value == 1 ether, "Must send 1 ETH to create a proposal");
+    function createProposal(
+        string memory title,
+        string memory description,
+        uint256 durationInMinutes,
+        uint8 quorumType
+    ) public payable {
+        require(msg.value == 1 ether, "Must send 1 ETH to create a proposal");
 
-    uint256 endTime = block.timestamp + (durationInMinutes * 1 minutes);
+        uint256 endTime = block.timestamp + (durationInMinutes * 1 minutes);
 
-    proposals.push(Proposal({
-        title: title,
-        description: description,
-        durationInMinutes: durationInMinutes,
-        quorumType: quorumType,
-        endTime: endTime,
-        proposer: msg.sender,
-        active: true,
-        votesFor: 0,
-        votesAgainst: 0
+        proposals.push(
+            Proposal({
+                title: title,
+                description: description,
+                durationInMinutes: durationInMinutes,
+                quorumType: quorumType,
+                endTime: endTime,
+                proposer: msg.sender,
+                active: true,
+                votesFor: 0,
+                votesAgainst: 0
+            })
+        );
 
-    }));
+        uint256 proposalId = proposals.length - 1;
+        proposalCount++;
 
-    uint256 proposalId = proposals.length - 1;
-    proposalCount++;
-
-    emit ProposalCreated(proposalId, title, description, durationInMinutes, quorumType, endTime, msg.sender);
-}
+        emit ProposalCreated(
+            proposalId,
+            title,
+            description,
+            durationInMinutes,
+            quorumType,
+            endTime,
+            msg.sender
+        );
+    }
 
     function addAdmin(address newAdmin) public onlyAdmin {
         admins[newAdmin] = true;
     }
 
-   function getProposals() public view returns (Proposal[] memory) {
-    return proposals;
-}
+    function getProposals() public view returns (Proposal[] memory) {
+        return proposals;
+    }
 
-
-    // Optional: Function to remove an admin dynamically
     function removeAdmin(address admin) public onlyAdmin {
         admins[admin] = false;
     }
 
-    // check if the user is an admin
     function isAdmin(address user) public view returns (bool) {
         return admins[user];
     }
-
 
     // function registerShareholder(address voter, uint256 shareCount) public {
     //     require(
@@ -149,9 +149,12 @@ function createProposal(
     // }
 
     function registerShareholder(address account, uint256 _shares) external {
-        require(!isVotingPeriodActive, "Cannot register shareholders during voting period");
+        require(
+            !isVotingPeriodActive,
+            "Cannot register shareholders during voting period"
+        );
         shares[account] = _shares;
-        tokens[account] = _shares;  // Convert shares to tokens (1:1 mapping)
+        tokens[account] = _shares; // Convert shares to tokens (1:1 mapping)
     }
 
     function vote(address account, uint256 voteAmount) external {
@@ -165,32 +168,33 @@ function createProposal(
         proposals[0].votesFor += voteAmount;
 
         // Record the vote
-        votingHistory[account].push(VotingRecord({
-            proposalId: 0,
-            votedFor: true,
-            voteWeight: voteAmount
-        }));
+        votingHistory[account].push(
+            VotingRecord({
+                proposalId: 0,
+                votedFor: true,
+                voteWeight: voteAmount
+            })
+        );
 
         emit VoteCast(0, account, voteAmount, true);
-
     }
 
     function getTokens(address account) external view returns (uint256) {
         return tokens[account];
     }
 
-    function calculateReward(
-        uint256 voteWeight
-    ) internal pure returns (uint256) {
+    function calculateReward(uint256 voteWeight) internal pure returns (uint256) {
         return voteWeight / 100;
     }
 
     function updateShares(address voter, uint256 newShares) public {
-        require(!isVotingPeriodActive, "Cannot update shares during voting period");
+        require(
+            !isVotingPeriodActive,
+            "Cannot update shares during voting period"
+        );
         shares[voter] = newShares;
         emit SharesUpdated(voter, newShares);
     }
-
 
     function getProposalOutcome(
         uint256 proposalId
@@ -202,32 +206,37 @@ function createProposal(
         return proposalCount;
     }
 
-function castVote(uint256 proposalId, bool voteFor) public {
-    require(!voted[msg.sender], "You have already voted");
-    require(proposals[proposalId].active, "Proposal is not active");
+    function castVote(uint256 proposalId, bool voteFor) public {
+        require(!voted[msg.sender], "You have already voted");
+        require(proposals[proposalId].active, "Proposal is not active");
 
-    uint256 voteWeight = calculateVotingWeight(msg.sender);
-    require(voteWeight > 0, "You must hold shares to vote");
+        uint256 voteWeight = calculateVotingWeight(msg.sender);
+        require(voteWeight > 0, "You must hold shares to vote");
 
-    if (voteFor) {
-        proposals[proposalId].votesFor += voteWeight;
-    } else {
-        proposals[proposalId].votesAgainst += voteWeight;
+        if (voteFor) {
+            proposals[proposalId].votesFor += voteWeight;
+        } else {
+            proposals[proposalId].votesAgainst += voteWeight;
+        }
+
+        votingHistory[msg.sender].push(
+            VotingRecord({
+                proposalId: proposalId,
+                votedFor: voteFor,
+                voteWeight: voteWeight
+            })
+        );
+
+        voted[msg.sender] = true;
+        isVotingPeriodActive = true;
+
+        uint256 rewardAmount = calculateReward(voteWeight);
+        governanceToken.mint(msg.sender, rewardAmount);
+        emit VoteCast(proposalId, msg.sender, voteWeight, voteFor);
+        emit RewardIssued(msg.sender, rewardAmount);
     }
 
-    votingHistory[msg.sender].push(VotingRecord({
-        proposalId: proposalId,
-        votedFor: voteFor,
-        voteWeight: voteWeight
-    }));
-
-    voted[msg.sender] = true;  
-    isVotingPeriodActive = true;
-
-    emit VoteCast(proposalId, msg.sender, voteWeight, voteFor);
-}
-
-event ProposalDeleted(uint256 indexed proposalId);
+    event ProposalDeleted(uint256 indexed proposalId);
 
     function deleteProposal(uint256 proposalId) external onlyAdmin {
         require(proposalId < proposals.length, "Invalid proposal ID");
@@ -235,47 +244,52 @@ event ProposalDeleted(uint256 indexed proposalId);
         emit ProposalDeleted(proposalId);
     }
 
-    function calculateVotingWeight(address voter) public view returns (uint256) {
+    function calculateVotingWeight(
+        address voter
+    ) public view returns (uint256) {
         require(totalShares > 0, "Total shares must be greater than zero");
         return (shares[voter] * 100) / totalShares;
     }
 
     function calculateQuorum(uint256 proposalId) public view returns (uint256) {
-    Proposal storage proposal = proposals[proposalId];
-    if (proposal.quorumType == uint8(QuorumType.SimpleMajority)) {
-        return totalShares / 2;
-    } else if (proposal.quorumType == uint8(QuorumType.TwoThirds)) {
-        return (totalShares * 2) / 3;
-    } else if (proposal.quorumType == uint8(QuorumType.ThreeQuarters)) {
-        return (totalShares * 3) / 4;
-    } else if (proposal.quorumType == uint8(QuorumType.Unanimous)) {
-        return totalShares;
-    } else {
-        revert("Invalid quorum type");
-    }
-}
-
-function finalizeProposal(uint256 proposalId) public {
-    Proposal storage proposal = proposals[proposalId];
-    require(block.timestamp >= proposal.endTime, "Voting period has not ended");
-    require(proposal.active, "Proposal is not active");
-
-    uint256 quorum = calculateQuorum(proposalId);
-    if (proposal.votesFor >= quorum) {
-        proposalOutcomes[proposalId] = ProposalOutcome({
-            passed: true,
-            votesFor: proposal.votesFor,
-            votesAgainst: proposal.votesAgainst
-        });
-    } else {
-        proposalOutcomes[proposalId] = ProposalOutcome({
-            passed: false,
-            votesFor: proposal.votesFor,
-            votesAgainst: proposal.votesAgainst
-        });
+        Proposal storage proposal = proposals[proposalId];
+        if (proposal.quorumType == uint8(QuorumType.SimpleMajority)) {
+            return totalShares / 2;
+        } else if (proposal.quorumType == uint8(QuorumType.TwoThirds)) {
+            return (totalShares * 2) / 3;
+        } else if (proposal.quorumType == uint8(QuorumType.ThreeQuarters)) {
+            return (totalShares * 3) / 4;
+        } else if (proposal.quorumType == uint8(QuorumType.Unanimous)) {
+            return totalShares;
+        } else {
+            revert("Invalid quorum type");
+        }
     }
 
-    proposal.active = false;
-    isVotingPeriodActive = false;
-}
+    function finalizeProposal(uint256 proposalId) public {
+        Proposal storage proposal = proposals[proposalId];
+        require(
+            block.timestamp >= proposal.endTime,
+            "Voting period has not ended"
+        );
+        require(proposal.active, "Proposal is not active");
+
+        uint256 quorum = calculateQuorum(proposalId);
+        if (proposal.votesFor >= quorum) {
+            proposalOutcomes[proposalId] = ProposalOutcome({
+                passed: true,
+                votesFor: proposal.votesFor,
+                votesAgainst: proposal.votesAgainst
+            });
+        } else {
+            proposalOutcomes[proposalId] = ProposalOutcome({
+                passed: false,
+                votesFor: proposal.votesFor,
+                votesAgainst: proposal.votesAgainst
+            });
+        }
+
+        proposal.active = false;
+        isVotingPeriodActive = false;
+    }
 }
